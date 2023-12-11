fd340832c7939cc5bf5b6ffce41ee6cdf8740e19
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2023-12-11 23:35:01 +0800

    app_py.py

--- app_py.py
+++ app_py.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
-from tensorflow.keras import callbacks,optimizers
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_v2_preprocess_input
+from tensorflow.keras import callbacks, optimizers
 import h5py
+import cv2  # Add this line
 
 @st.cache(allow_output_mutation=True)
 def load_model():
+    # Add your model loading logic here
     h5 = h5py.File(f)
     return tf.keras.models.load_model(h5)
 
@@ -17,17 +19,17 @@ st.write("""
 
 file = st.file_uploader("Choose item images from computer", type=["jpg", "png"])
 
-import cv2
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 import numpy as np
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
 def import_and_predict(image_data, model):
     size = (224, 224)    
     image_object = ImageOps.fit(image_data, size, Image.LANCZOS)
     image_array = np.asarray(image_object)
     image_cv = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
     img_reshape = image_cv[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+    prediction = model.predict(img_reshape)
     return prediction
 
 if file is None:
@@ -36,6 +38,6 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     predictions = import_and_predict(image, load_model())
-    class_names = ['Vegetables','Packages', 'Fruits']
-    string="This image is: "+class_names[np.argmax(predictions)]
+    class_names = ['Vegetables', 'Packages', 'Fruits']
+    string = "This image is: " + class_names[np.argmax(predictions)]
     st.success(string)
