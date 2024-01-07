3492c1fffce56d2b77617b3b375fd0a665ac637d
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-01-07 16:46:30 +0800

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model_path = 'best_model.hdf5'
+    model_path = 'weights.best.hdf5'
     try:
         model = tf.keras.models.load_model(model_path, compile=False)
         return model
