fc7e0c5ec87fd5746e7c92698461d896807e2610
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-01-07 17:02:12 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,54 +1,32 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image, ImageOps  # Add this line
-import numpy as np
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model_path = 'weights.best.hdf5'
-    try:
-        model = tf.keras.models.load_model(model_path, compile=False)
-        return model
-    except Exception as e:
-        st.error(f"Error loading the model: {e}")
-        return None
+  model=tf.keras.models.load_model('weights.best.hdf5')
+  return model
+model=load_model()
+st.write("""
+# Welcome to the World of Lego Toys"""
+)
+file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 
-def import_and_predict(image_data, model):
-    size = (64, 64)
-    
-    try:
-        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    except AttributeError:
-        st.error("Error processing the image. Please try again with a different image.")
-        return None
-    
-    img = np.asarray(image)
-    img_reshape = img[np.newaxis, ...]
-    
-    try:
-        prediction = model.predict(img_reshape)
-        return prediction
-    except Exception as e:
-        st.error(f"Error making predictions: {e}")
-        return None
-
-def main():
-    st.write("# World of Lego toys classifier")
-
-    file = st.file_uploader("Upload a Lego model from your computer", type=["jpg", "png"])
-
-    if file is None:
-        st.text("Please upload an image file")
-    else:
-        model = load_model()
-        if model is not None:
-            image = Image.open(file)
-            st.image(image, use_column_width=True)
-            prediction = import_and_predict(image, model)
-            if prediction is not None:
-                class_names = ['harry-potter', 'starwars', 'jurassic-world', 'marvel']
-                output_class = class_names[np.argmax(prediction)]
-                st.success(f"OUTPUT: {output_class}")
-
-if __name__ == "__main__":
-    main()
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['harry-potter,'jurassic-park','marvel','star-wars']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
