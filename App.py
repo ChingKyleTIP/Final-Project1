af67997e5a4e0ab7be85a3b9c06def81a67b523e
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-01-07 16:03:33 +0800

    Update App.py

--- App.py
+++ App.py
@@ -57,4 +57,5 @@ def main():
                 output_class = class_names[np.argmax(prediction)]
                 st.success(f"OUTPUT: {output_class}")
                 
- if __name__ == '__main__': run()
+if __name__ == "__main__":
+    main()
