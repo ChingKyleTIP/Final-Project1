f9dd95e7d4a29cdcef8b62c83a06cc7bae327fbb
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-01-09 16:12:41 +0800

    Update App22.py

--- App22.py
+++ App22.py
@@ -23,17 +23,22 @@ def import_and_predict(image_data, model):
         image = Image.open(image_data)
         # Resize the image to (64, 64) using TensorFlow function
         image = tf.image.resize(np.array(image), size)
-        
+
         # Convert the image to grayscale using TensorFlow function
         image = tf.image.rgb_to_grayscale(image)
 
         img_array = tf.image.convert_image_dtype(image, tf.float32)
         img_array = tf.expand_dims(img_array, 0)
+        
+        # Reshape the image array to match the model input shape
+        img_array = tf.image.resize_with_crop_or_pad(img_array, target_height=64, target_width=64)
+        
         prediction = model.predict(img_array)
         return prediction
     except Exception as e:
         st.error(f"Error processing the image: {str(e)}")
         return None
+
 
 if file is None:
     st.text("Please upload an image file.")
