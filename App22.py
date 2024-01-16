039b49b9d322d7d90b5f115c4c305204c20b6920
ChingKyleTIP <153418736+ChingKyleTIP@users.noreply.github.com>
2024-01-16 20:43:59 +0800

    Update App22.py

--- App22.py
+++ App22.py
@@ -14,7 +14,7 @@ st.write("""
 # Welcome to the World of Lego Toys
 """)
 
-file = st.file_uploader("Choose a toy photo from your computer", type=["jpg"])
+file = st.file_uploader("Choose a toy photo from your computer", type=["jpg", "png"])
 
 def import_and_predict(image_data, model):
     size = (64, 64)
